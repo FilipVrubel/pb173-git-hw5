e56c30900bfcd2ed23e9d5b9c626287115059ded
Filip Vrubel <114007566+FilipVrubel@users.noreply.github.com>
2026-04-14 15:08:48 +0200

    fix: check fmt.Scan for error

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func add(a, b int) int {
@@ -11,6 +12,10 @@ func add(a, b int) int {
 func main() {
 	var a, b int
 	fmt.Print("Enter two numbers: ")
-	fmt.Scan(&a, &b)
+	_, err := fmt.Scan(&a, &b)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+		os.Exit(1)
+	}
 	fmt.Printf("%d + %d = %d\n", a, b, add(a, b))
 }
